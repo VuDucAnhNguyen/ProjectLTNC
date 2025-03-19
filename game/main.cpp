57add4cc3dc5652d848b09f3ec656e9fe1e078a9
VuDucAnhNguyen <vuducanhnguyenws@gmail.com>
2025-03-19 21:05:40 +0700

    add background + basket

--- game/main.cpp
+++ game/main.cpp
@@ -6,26 +6,70 @@ using namespace std;
 bool init ();
 bool loadMedia ();
 void close ();
+SDL_Surface* loadSurface (string path );
+SDL_Surface* gScreenSurface = NULL;
+
+enum KeyPressSurfaces{
+    KEY_PRESS_SURFACE_DEFAULT,
+    KEY_PRESS_SURFACE_UP,
+    KEY_PRESS_SURFACE_DOWN,
+    KEY_PRESS_SURFACE_LEFT,
+    KEY_PRESS_SURFACE_RIGHT,
+    KEY_PRESS_SURFACE_TOTAL
+};
 
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
 SDL_Window* window = NULL;
 SDL_Surface* screenSurface = NULL;
 SDL_Surface* HelloWorld = NULL;
+SDL_Surface* currentSurface=NULL;
+SDL_Surface* KeyPressSurfaces[ KEY_PRESS_SURFACE_TOTAL ];
+
 
 int main(int argc, char* argv[]){
+
+
     if (!init()){
         cout <<"Failed to initialize\n";
-    } else {
-        if( !loadMedia() )
-        {
-            cout<< "Failed to load media!\n" ;
-        } else {
-            SDL_BlitSurface( HelloWorld, NULL, screenSurface, NULL );
-            SDL_UpdateWindowSurface(window);
-            SDL_Event e; bool quit = false; while( quit == false ){ while( SDL_PollEvent( &e ) ){ if( e.type == SDL_QUIT ) quit = true; } }
+    } else if (!loadMedia()){
+        cout << "Failed to load media!\n";
+       } else {
+        SDL_Event e;
+        bool quit = false;
+        while( quit == false ){
+            while( SDL_PollEvent(&e)){
+                if( e.type == SDL_QUIT ){
+                    quit = true;
+                } else if (SDL_KEYDOWN){
+                    switch (e.key.keysym.sym){
+                        case SDLK_UP:
+                        currentSurface = KeyPressSurfaces[ KEY_PRESS_SURFACE_UP ];
+                        break;
+
+                        case SDLK_DOWN:
+                        currentSurface = KeyPressSurfaces[ KEY_PRESS_SURFACE_DOWN ];
+                        break;
+
+                        case SDLK_LEFT:
+                        currentSurface = KeyPressSurfaces[ KEY_PRESS_SURFACE_LEFT ];
+                        break;
+
+                        case SDLK_RIGHT:
+                        currentSurface = KeyPressSurfaces[ KEY_PRESS_SURFACE_RIGHT ];
+                        break;
+
+                        default:
+                        currentSurface = KeyPressSurfaces[ KEY_PRESS_SURFACE_DEFAULT ];
+                        break;
+                    }
+                    SDL_BlitSurface( currentSurface, NULL, screenSurface, NULL );
+                    SDL_UpdateWindowSurface( window );
+                }
+            }
         }
     }
+
     close();
     return 0;
 }
@@ -49,17 +93,56 @@ bool init (){
 
 bool loadMedia (){
     bool success=true;
-    HelloWorld=SDL_LoadBMP("image_project/preview.bmp");
-    if (HelloWorld==NULL){
-        cout <<"Unable load image "<<SDL_GetError();
-        success=false;
+    KeyPressSurfaces[ KEY_PRESS_SURFACE_DEFAULT]=loadSurface("image_project/press.bmp");
+    if( KeyPressSurfaces[ KEY_PRESS_SURFACE_DEFAULT ] == NULL )
+    {
+        cout << "Failed load default image";
+        success = false;
+    }
+
+    KeyPressSurfaces[ KEY_PRESS_SURFACE_UP]=loadSurface("image_project/up.bmp");
+    if( KeyPressSurfaces[ KEY_PRESS_SURFACE_UP ] == NULL )
+    {
+        cout << "Failed load up image";
+        success = false;
+    }
+
+    KeyPressSurfaces[ KEY_PRESS_SURFACE_DOWN]=loadSurface("image_project/down.bmp");
+    if( KeyPressSurfaces[ KEY_PRESS_SURFACE_DOWN ] == NULL )
+    {
+        cout << "Failed load down image";
+        success = false;
+    }
+
+    KeyPressSurfaces[ KEY_PRESS_SURFACE_LEFT]=loadSurface("image_project/left.bmp");
+    if( KeyPressSurfaces[ KEY_PRESS_SURFACE_LEFT ] == NULL )
+    {
+        cout << "Failed load left image";
+        success = false;
+    }
+
+    KeyPressSurfaces[ KEY_PRESS_SURFACE_RIGHT]=loadSurface("image_project/right.bmp");
+    if( KeyPressSurfaces[ KEY_PRESS_SURFACE_RIGHT ] == NULL )
+    {
+        cout << "Failed load right image";
+        success = false;
     }
     return success;
 }
 
+SDL_Surface* loadSurface (string path){
+    SDL_Surface* loadedSurface = SDL_LoadBMP(path.c_str());
+    if ( loadedSurface==NULL){
+        cout <<"Unable load image "<<SDL_GetError();
+    }
+    return loadedSurface;
+}
+
 void close (){
-    SDL_FreeSurface(HelloWorld);
-    HelloWorld=NULL;
+    for (int i = 0; i < KEY_PRESS_SURFACE_TOTAL; ++i) {
+        SDL_FreeSurface(KeyPressSurfaces[i]);
+        KeyPressSurfaces[i] = NULL;
+    }
 
     SDL_DestroyWindow(window);
     window=NULL;
